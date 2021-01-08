cdfc0b888617fe73c964ccc01665ff329c0d5883
Yurii Holiuk <bionicuss@gmail.com>
2021-01-08 16:26:05 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 // write code here
+
+const list = document.querySelector('ul');
+const items = [...list.querySelectorAll('li')];
+
+const getSalary = li => parseInt(li.dataset.salary.replace(/[,$]/, ''));
+
+items.sort((a, b) => {
+  return -(getSalary(a) - getSalary(b));
+});
+
+list.append(...items);
